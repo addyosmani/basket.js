dfa762ec4e15fcbe1985b7f34cc5fce30d2ff03f
addyosmani <addyosmani@gmail.com>
2012-02-29 22:43:05 +0000

    initial version

---
+++ README.md
@@ -0,0 +1,1 @@
+basket.js - Script loader for caching scripts using localStorage
---
+++ basket.js
@@ -0,0 +1,121 @@
+
+/* Basket.js
+ * A script-loader that handles caching scripts in localStorage
+ * where supported.
+ * http://addyosmani.com/
+ * Credits: Addy Osmani, Mathias Bynens, Ironsjp.
+ * Copyright (c) 2012 Addy Osmani; 
+ * Licensed MIT, GPL 
+ */
+
+; (function (w, d) {
+
+    function basketLoader() {
+
+        var
+            _storagePrefix = "ll-",
+            _localStorage = function (a, b) {
+                try {
+                    return (a = localStorage).setItem(b, a), a.removeItem(b), !0
+                } catch (c) {}
+            }(),
+            scripts = [],
+            scriptsExecuted = 0,
+            waitCount = 0,
+            waitCallbacks = [],
+
+
+            // Minimalist Cross-browser XHR
+            // from https://gist.github.com/991713
+            getXMLObj = function (s, a) {
+                a = [a = "Msxml2.XMLHTTP", a + ".3.0", a + ".6.0"];
+                do
+                try {
+                    s = a.pop();
+                    return new(s ? ActiveXObject : XMLHttpRequest)(s)
+                } catch (e) {;
+                }
+                while (s)
+            },
+
+            getUrl = function (url, callback) {
+                var xhr = getXMLObj();
+                xhr.open("GET", url, true);
+                xhr.onreadystatechange = function (e) {
+                    if (xhr.readyState === 4) {
+                        callback(xhr.responseText);
+                    }
+                };
+                xhr.send();
+            },
+
+            injectScript = function (text) {
+                var script = d.createElement("script"),
+                    head = d.head || d.getElementsByTagName("head")[0];
+                script.appendChild(d.createTextNode(text));
+                head.insertBefore(script, head.firstChild);
+            },
+
+            queueExec = function (waitCount) {
+                var script, i, j, callback;
+
+                if (scriptsExecuted >= waitCount) {
+                    for (i = 0; i < scripts.length; i++) {
+                        script = scripts[i];
+                        if (!script) {
+                            // loading/executed
+                            continue;
+                        }
+                        scripts[i] = null;
+                        injectScript(script);
+                        scriptsExecuted++;
+
+                        for (j = i; j < scriptsExecuted; j++) {
+                            if (callback = waitCallbacks[j]) {
+                                waitCallbacks[j] = null;
+                                callback();
+                            }
+                        }
+                    }
+                }
+            };
+
+        return {
+
+            add: function (path) {
+
+                var key = _storagePrefix + path,
+                    scriptIndex = scripts.length,
+                    _waitCount = waitCount;
+
+                scripts[scriptIndex] = null;
+
+                if (_localStorage && _localStorage.getItem(key)) {
+                    scripts[scriptIndex] = _localStorage.getItem(key);
+                    queueExec(_waitCount);
+                } else {
+                    getUrl(path, function (text) {
+                        (_localStorage) && _localStorage.setItem(key, text);
+                        scripts[scriptIndex] = text;
+                        queueExec(_waitCount);
+                    });
+                }
+                return this;
+            },
+
+            wait: function (callback) {
+                waitCount = scripts.length;
+
+                if (callback) {
+                    (scriptsExecuted >= waitCount - 1) ? callback() : waitCallbacks[waitCount - 1] = callback;
+                }
+                return this;
+            }
+        };
+
+    }
+
+
+    w['basket'] = basketLoader();
+
+})(this, document);
