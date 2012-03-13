2a7e7d66eb64ad3fba7276ed4318e1d41fe6740f
Andrée Hansson <peolanha@gmail.com>
2012-03-13 22:59:47 +0100

    Initial API rewrite, removed `.add()`, added `.require(obj1, obj2)` with support for individual `wait` parameters in `objN` or all with `.wait()`. Also support for `execute: false` parameter.

---
+++ test/fail-script.js
@@ -0,0 +1,1 @@
+basket.fail = true;
--- test/tests.js
+++ test/tests.js
@@ -1,143 +1,89 @@
-(function() {
 module("Test script API", {
 	teardown: function() {
 		localStorage.clear();
+		basket.fail = false;
 	}
 });
 
 
-asyncTest("require() a script, execute callback post-load via wait()", 4, function(){
-	basket
-		.require("jquery-1.7.1.min.js")
-		.wait(function(){
-			ok(true, "first callback invoked");
-			ok(
-				localStorage.getItem("basket-jquery-1.7.1.min.js"),
-				"file saved in local storage correctly"
-			);
-			start();
-		})
-		.require("modernizr.js")
-		.wait(function(){
-			ok(true, "second callback invoked");
-			ok(
-				localStorage.getItem("basket-modernizr.js"),
-				"file saved in local storage correctly"
-			);
-			start();
-		});
-});
-
-
-asyncTest("add() scripts without injection", 1, function() {
-	basket
-		.add("jquery-1.7.1.min.js")
-		.add("modernizr.js");
-
-	setTimeout(function() {
-		var testItem = window.localStorage.getItem("basket-modernizr.js");
-		ok(testItem.length >0 , "ensuring items reloaded contain content");
+asyncTest( "require() 1 script", 2, function() {
+	var cancel = setTimeout(function() {
+		ok( false, "Callback never invoked." );
+		ok( false );
 		start();
-	}, 100);
-});
+	}, 2500);
 
+	basket.require({
+		url: "jquery-1.7.1.min.js" ,
+		wait: function() {
+			clearTimeout( cancel );
 
-asyncTest("remove() scripts from storage", 2, function() {
-		// Add initial scripts to storage
-		basket
-			.add("jquery-1.7.1.min.js")
-			.add("modernizr.js");
+			ok( true, "Callback invoked" );
+			ok( basket.get("jquery-1.7.1.min.js"), "Data exists in localStorage" );
 
-		// If the previous test passed, we know add() works
-		// lets assume there should be two items in LS now.
-		// Remove them.
-
-		setTimeout(function() {
-			basket
-				.remove("jquery-1.7.1.min.js")
-				.remove("modernizr.js");
-
-			var testjQuery = window.localStorage.getItem("jquery-1.7.1.min.js");
-			var testModernizr = window.localStorage.getItem("basket-modernizr.js");
-			strictEqual(!!testModernizr, false, "ensure script 1 removed");
-			strictEqual(!!testjQuery, false, "ensure script 2 removed");
 			start();
-
-		}, 100);
-});
-
-
-asyncTest("Chaining require() calls", 1, function() {
-	basket
-		.require("jquery-1.7.1.min.js")
-		.require("underscore-min.js")
-		.require("backbone-min.js").wait(function() {
-			strictEqual(window.localStorage.length, 3, "Items correctly added via chaining");
-			start();
+		}
 	});
 });
 
 
-asyncTest("require() multiple scripts, test scripts loaded and can be used", 3, function() {
-	basket
-		.require("jquery-1.7.1.min.js").wait(function() {
-			var el = $("ul");
-			strictEqual(el.length, 1, "Selected element correctly using jQuery");
-			start();
-		});
+asyncTest( "require() 2 scripts with .wait()", 3, function() {
+	var cancel = setTimeout(function() {
+		ok( false, "Callback never invoked." );
+		ok( false );
+		ok( false );
+		start();
+	}, 2500);
 
-	stop();
+	basket.require(
+			{ url: "jquery-1.7.1.min.js" },
+			{ url: "modernizr.js" }
+		)
+		.wait(function() {
+			clearTimeout( cancel );
 
-	basket
-		.require("underscore-min.js").wait(function() {
-			var sum = _.reduce([1, 2, 3], function(memo, num) { return memo + num; }, 0);
-			strictEqual(sum, 6, "Sum operation via Underscore processed correctly");
-			start();
-		});
+			ok( true, "Callback invoked" );
+			ok( basket.get("jquery-1.7.1.min.js"), "Data exists in localStorage" );
+			ok( basket.get("modernizr.js"), "Data exists in localStorage" );
 
-	stop();
-
-	basket
-		.require("backbone-min.js").wait(function() {
-			var Meal = Backbone.Model.extend({
-				idAttribute: "_id"
-			});
-
-			var cake = new Meal({ _id: 4, name: "Cake" });
-			strictEqual(cake.id, 4, "Retrieved id of Backbone.Model instance");
 			start();
 		});
 });
 
 
-module("Test localStorage population");
+asyncTest( "require() 2 scripts (one non-executed) with .wait()", 4, function() {
+	var cancel = setTimeout(function() {
+		ok( false, "Callback never invoked." );
+		ok( false );
+		ok( false );
+		start();
+	}, 2500);
 
+	basket.require(
+			{ url: "fail-script.js", execute: false },
+			{ url: "modernizr.js" }
+		)
+		.wait(function() {
+			clearTimeout( cancel );
 
-asyncTest("require() cache scripts to localStorage, scripts can be accessed and contain expected contents", 2, function() {
-	basket
-		.require("modernizr.js")
-		.require("underscore-min.js").wait(function() {
-			var test = localStorage.getItem("basket-modernizr.js");
-			ok(test.length > 0, "script contents are greater than 0");
-			ok(
-				test.indexOf("applicationcache-canvas-canvastext") > 0,
-				"script contents contain expected strings"
-			);
+			ok( true, "Callback invoked" );
+			ok( basket.get("modernizr.js"), "Data exists in localStorage" );
+			ok( basket.get("fail-script.js"), "Data exists in localStorage" );
+			ok( basket.fail !== true, "Script not executed" );
+
 			start();
 		});
 });
 
 
-module("Utilities");
+asyncTest( "require(), custom key", 1, function() {
+	var key = +new Date();
 
+	basket
+		.require({ url: "jquery-1.7.1.min.js", key: key })
+		.wait(function() {
+			ok( basket.get(key), "Data exists in localStorage under custom key" );
 
-test("Can correctly detect if input is a function or not. Used for callbacks", function() {
-	expect(2);
-	// isFunc() logic abstracted from the private section of basket.js
-	function method(){};
-	var variable = "test";
-	strictEqual(Object.prototype.toString.call(method) === "[object Function]", true);
-	strictEqual(Object.prototype.toString.call(variable) === "[object Function]", false);
+			start();
+		});
 });
-
-}());
