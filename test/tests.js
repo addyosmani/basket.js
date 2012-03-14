0df4d29dbde0b1cc3ee50c1370cc13161fe0249f
Sindre Sorhus <sindresorhus@gmail.com>
2012-03-14 23:28:15 +0100

    Convert tests.js to single-quotes

--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,4 @@
-module("Test script API", {
+module( 'Test script API', {
 	teardown: function() {
 		localStorage.clear();
 		basket.fail = false;
@@ -6,20 +6,20 @@ module("Test script API", {
 });
 
 
-asyncTest( "require() 1 script", 2, function() {
+asyncTest( 'require() 1 script', 2, function() {
 	var cancel = setTimeout(function() {
-		ok( false, "Callback never invoked." );
+		ok( false, 'Callback never invoked' );
 		ok( false );
 		start();
 	}, 2500);
 
 	basket.require({
-		url: "jquery-1.7.1.min.js" ,
+		url: 'jquery-1.7.1.min.js' ,
 		wait: function() {
 			clearTimeout( cancel );
 
-			ok( true, "Callback invoked" );
-			ok( basket.get("jquery-1.7.1.min.js"), "Data exists in localStorage" );
+			ok( true, 'Callback invoked' );
+			ok( basket.get('jquery-1.7.1.min.js'), 'Data exists in localStorage' );
 
 			start();
 		}
@@ -27,62 +27,62 @@ asyncTest( "require() 1 script", 2, function() {
 });
 
 
-asyncTest( "require() 2 scripts with .wait()", 3, function() {
+asyncTest( 'require() 2 scripts with .wait()', 3, function() {
 	var cancel = setTimeout(function() {
-		ok( false, "Callback never invoked." );
+		ok( false, 'Callback never invoked' );
 		ok( false );
 		ok( false );
 		start();
 	}, 2500);
 
 	basket.require(
-			{ url: "jquery-1.7.1.min.js" },
-			{ url: "modernizr.js" }
+			{ url: 'jquery-1.7.1.min.js' },
+			{ url: 'modernizr.js' }
 		)
 		.wait(function() {
 			clearTimeout( cancel );
 
-			ok( true, "Callback invoked" );
-			ok( basket.get("jquery-1.7.1.min.js"), "Data exists in localStorage" );
-			ok( basket.get("modernizr.js"), "Data exists in localStorage" );
+			ok( true, 'Callback invoked' );
+			ok( basket.get('jquery-1.7.1.min.js'), 'Data exists in localStorage' );
+			ok( basket.get('modernizr.js'), 'Data exists in localStorage' );
 
 			start();
 		});
 });
 
 
-asyncTest( "require() 2 scripts (one non-executed) with .wait()", 4, function() {
+asyncTest( 'require() 2 scripts (one non-executed) with .wait()', 4, function() {
 	var cancel = setTimeout(function() {
-		ok( false, "Callback never invoked." );
+		ok( false, 'Callback never invoked' );
 		ok( false );
 		ok( false );
 		start();
 	}, 2500);
 
 	basket.require(
-			{ url: "fail-script.js", execute: false },
-			{ url: "modernizr.js" }
+			{ url: 'fail-script.js', execute: false },
+			{ url: 'modernizr.js' }
 		)
 		.wait(function() {
 			clearTimeout( cancel );
 
-			ok( true, "Callback invoked" );
-			ok( basket.get("modernizr.js"), "Data exists in localStorage" );
-			ok( basket.get("fail-script.js"), "Data exists in localStorage" );
-			ok( basket.fail !== true, "Script not executed" );
+			ok( true, 'Callback invoked' );
+			ok( basket.get('modernizr.js'), 'Data exists in localStorage' );
+			ok( basket.get('fail-script.js'), 'Data exists in localStorage' );
+			ok( basket.fail !== true, 'Script not executed' );
 
 			start();
 		});
 });
 
 
-asyncTest( "require(), custom key", 1, function() {
+asyncTest( 'require(), custom key', 1, function() {
 	var key = +new Date();
 
 	basket
-		.require({ url: "jquery-1.7.1.min.js", key: key })
+		.require({ url: 'jquery-1.7.1.min.js', key: key })
 		.wait(function() {
-			ok( basket.get(key), "Data exists in localStorage under custom key" );
+			ok( basket.get(key), 'Data exists in localStorage under custom key' );
 
 			start();
 		});
