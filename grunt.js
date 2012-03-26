f5a53ba8e3747bd201a2107e4c7ecf8b18e9bf60
Sindre Sorhus <sindresorhus@gmail.com>
2012-03-27 00:34:17 +0200

    Make build script compatible with latest grunt 0.3

--- grunt.js
+++ grunt.js
@@ -1,51 +1,62 @@
-/*global config:true, task:true*/
-config.init({
-	pkg: '<json:package.json>',
-	meta: {
-		banner: '/*! <%= pkg.name %>\n' +
-			'* v<%= pkg.version %> - ' +
-			'<%= template.today("yyyy-mm-dd") %>\n' +
-			'<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-			'* (c) <%= pkg.author.name %>;' +
-			' <%= _.pluck(pkg.licenses, "type").join(", ") %> License\n' +
-			'* Created by: <%= _.pluck(pkg.maintainers, "name").join(", ") %>\n' +
-			'* Contributors: <%= _.pluck(pkg.contributors, "name").join(", ") %> */'
-	},
-	concat: {
-		'dist/basket.js': ['<banner>', '<file_strip_banner:lib/basket.js>']
-	},
-	min: {
-		'dist/basket.min.js': ['<banner>', 'dist/basket.js']
-	},
-	lint: {
-		files: ['grunt.js', 'lib/**/*.js']
-	},
-	qunit: {
-		index: ['test/index.html']
-	},
-	watch: {
-		files: '<config:lint.files>',
-		tasks: 'lint test'
-	},
-	jshint: {
-		options: {
-			curly: true,
-			eqeqeq: true,
-			immed: true,
-			latedef: true,
-			newcap: true,
-			noarg: true,
-			sub: true,
-			undef: true,
-			boss: true,
-			eqnull: true,
-			expr: true
+module.exports = function( grunt ) {
+	'use strict';
+
+	grunt.initConfig({
+		pkg: '<json:package.json>',
+		meta: {
+			banner: '/*!\n' +
+				'* <%= pkg.name %>\n' +
+				'* v<%= pkg.version %> - ' +
+				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+				'<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+				'* (c) <%= pkg.author.name %>;' +
+				' <%= _.pluck(pkg.licenses, "type").join(", ") %> License\n' +
+				'* Created by: <%= _.pluck(pkg.maintainers, "name").join(", ") %>\n' +
+				'* Contributors: <%= _.pluck(pkg.contributors, "name").join(", ") %>\n*/'
+		},
+		concat: {
+			'dist/basket.js': ['<banner>', '<file_strip_banner:lib/basket.js>']
+		},
+		min: {
+			'dist/basket.min.js': ['<banner>', 'dist/basket.js']
+		},
+		lint: {
+			files: ['grunt.js', 'lib/**/*.js']
+		},
+		qunit: {
+			index: ['test/index.html']
+		},
+		watch: {
+			files: '<config:lint.files>',
+			tasks: 'lint test'
+		},
+		jshint: {
+			options: {
+				es5: true,
+				esnext: true,
+				bitwise: true,
+				curly: true,
+				eqeqeq: true,
+				newcap: true,
+				noarg: true,
+				noempty: true,
+				regexp: true,
+				undef: true,
+				strict: true,
+				trailing: true,
+				smarttabs: true,
+				browser: true,
+				node: true,
+				nonstandard: true,
+				expr: true
+			}
 		}
-	}
-});
+	});
 
-// Dev - default
-task.registerTask('default', 'lint qunit');
+	// Dev - default
+	grunt.registerTask('default', 'lint concat min');
 
-// Release
-task.registerTask('release', 'lint qunit concat min');
+	// Release
+	grunt.registerTask('release', 'lint qunit concat min');
+
+};
