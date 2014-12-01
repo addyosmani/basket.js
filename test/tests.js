7833d117d49cffdf44532f8fa52bbfb45d6cd258
Thiago Lagden <lagden@gmail.com>
2014-12-01 13:04:23 -0200

    just for travis test

--- test/tests.js
+++ test/tests.js
@@ -676,6 +676,32 @@ asyncTest( 'with skipCache: true, we do not cache data', 1, function() {
 		});
 });
 
+// asyncTest( 'execute a cached script when execute: true', 2, function() {
+// 	var cancel = setTimeout(function() {
+// 		ok( false, 'Callback never invoked' );
+// 		start();
+// 	}, 2500);
+
+// 	function requireScript(execute, cb) {
+// 		basket.require(
+// 			{ url: 'fixtures/noexecute.js', execute: execute }
+// 		)
+// 		.then(cb);
+// 	}
+
+// 	requireScript( false, function() {
+// 		clearTimeout( cancel );
+
+// 		ok( typeof basket.executed === 'undefined', 'None-cached script was not executed' );
+
+// 		requireScript( true, function() {
+// 			ok( basket.executed === true, 'Cached script executed' );
+
+// 			start();
+// 		});
+// 	});
+// });
+
 asyncTest( 'compressed script', 1, function() {
 	basket.clear();
 	basket
@@ -685,30 +711,4 @@ asyncTest( 'compressed script', 1, function() {
 				ok( basket.get( 'largeScript' ) , 'Script added' );
 				start();
 		});
-});
-
-asyncTest( 'execute a cached script when execute: true', 2, function() {
-	var cancel = setTimeout(function() {
-		ok( false, 'Callback never invoked' );
-		start();
-	}, 4000);
-
-	function requireScript(execute, cb) {
-		basket.require(
-			{ url: 'fixtures/noexecute.js', execute: execute }
-		)
-		.then(cb);
-	}
-
-	requireScript( false, function() {
-		clearTimeout( cancel );
-
-		ok( typeof basket.executed === 'undefined', 'None-cached script was not executed' );
-
-		requireScript( true, function() {
-			ok( basket.executed === true, 'Cached script executed' );
-
-			start();
-		});
-	});
 });
